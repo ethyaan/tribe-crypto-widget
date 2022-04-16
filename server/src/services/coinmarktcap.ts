7053979ca35f88d51882d0d8e04816fbd25daa95
Ehsan Aghaei <ehsan.aghaeii@gmail.com>
2022-04-17 00:45:07 +0300

    implement gainer & looser refactor the way of calling axios

--- server/src/services/coinmarktcap.ts
+++ server/src/services/coinmarktcap.ts
@@ -2,102 +2,38 @@ import axios from "axios";
 import { CMC_API_URL, CMC_KEY } from "@config"
 import { logger } from '@/utils/logger';
 
-// const Response = {
-//     "data": [
-//     {
-//     "id": 1,
-//     "name": "Bitcoin",
-//     "symbol": "BTC",
-//     "slug": "bitcoin",
-//     "cmc_rank": 1,
-//     "num_market_pairs": 500,
-//     "circulating_supply": 17200062,
-//     "total_supply": 17200062,
-//     "max_supply": 21000000,
-//     "last_updated": "2018-06-02T22:51:28.209Z",
-//     "date_added": "2013-04-28T00:00:00.000Z",
-//     "tags": [
-//     "mineable"
-//     ],
-//     "platform": null,
-//     "quote": {
-//     "USD": {
-//     "price": 9283.92,
-//     "volume_24h": 7155680000,
-//     "percent_change_1h": -0.152774,
-//     "percent_change_24h": 0.518894,
-//     "percent_change_7d": 0.986573,
-//     "market_cap": 158055024432,
-//     "last_updated": "2018-08-09T22:53:32.000Z"
-//     },
-//     "BTC": {
-//     "price": 1,
-//     "volume_24h": 772012,
-//     "percent_change_1h": 0,
-//     "percent_change_24h": 0,
-//     "percent_change_7d": 0,
-//     "market_cap": 17024600,
-//     "last_updated": "2018-08-09T22:53:32.000Z"
-//     }
-//     }
-//     },
-//     {
-//     "id": 1027,
-//     "name": "Ethereum",
-//     "symbol": "ETH",
-//     "slug": "ethereum",
-//     "num_market_pairs": 6089,
-//     "circulating_supply": 17200062,
-//     "total_supply": 17200062,
-//     "max_supply": 21000000,
-//     "last_updated": "2018-06-02T22:51:28.209Z",
-//     "date_added": "2013-04-28T00:00:00.000Z",
-//     "tags": [
-//     "mineable"
-//     ],
-//     "platform": null,
-//     "quote": {
-//     "USD": {
-//     "price": 1678.6501384942708,
-//     "volume_24h": 7155680000,
-//     "percent_change_1h": -0.152774,
-//     "percent_change_24h": 0.518894,
-//     "percent_change_7d": 0.986573,
-//     "market_cap": 158055024432,
-//     "last_updated": "2018-08-09T22:53:32.000Z"
-//     },
-//     "ETH": {
-//     "price": 1,
-//     "volume_24h": 772012,
-//     "percent_change_1h": 0,
-//     "percent_change_24h": 0,
-//     "percent_change_7d": 0,
-//     "market_cap": 17024600,
-//     "last_updated": "2018-08-09T22:53:32.000Z"
-//     }
-//     }
-//     }
-//     ],
-//     "status": {
-//     "timestamp": "2019-04-02T22:44:24.200Z",
-//     "error_code": 0,
-//     "error_message": "",
-//     "elapsed": 10,
-//     "credit_count": 1
-//     }
-//     };
+const apiRoutes = {
+    'listings-latest'   : '/v1/cryptocurrency/listings/latest',
+    'quotes-latest'     : '/v2/cryptocurrency/quotes/latest',
+    'gainers-losers'    : '/v1/cryptocurrency/trending/gainers-losers'
+};
 
-export const getTop10 = () => {
-    return new Promise(async(resolve, reject) => {
+enum sortDirection {
+    gainer = 'asc',
+    looser = 'desc'
+}
+
+const call = (rouetName: string, queryString: string, basePair = 'USD') => {
+    const route = apiRoutes[rouetName];
+    if(!route) return;
+    return axios.get(
+        `${CMC_API_URL}${route}${queryString}`,
+        { headers: { 'X-CMC_PRO_API_KEY': CMC_KEY } }
+    );
+};
+
+
+/**
+ * get top 10 from coin market cap based on cmc_rank and format as simpler object
+ * @returns 
+ */
+export const getTop10 = (): Promise<any> => {
+    return new Promise(async(resolve, reject): Promise<any> => {
         try {
             let response = [];
             let basePair = `USD`;
-            let { data: { data: responseData }} = await axios.get(
-                `${CMC_API_URL}/v1/cryptocurrency/listings/latest?start=1&limit=10&convert=${basePair}`,
-                { headers: { 'X-CMC_PRO_API_KEY': CMC_KEY } }
-            );
-
-            responseData.forEach(({ symbol, quote }) => {
+            let { data: { data }} = await call('listings-latest', `?start=1&limit=10&convert=${basePair}`);
+            data.forEach(({ symbol, quote }) => {
                 const { price, percent_change_24h } = quote[basePair];
                 response.push({
                     symbol: `${symbol}/${basePair}`,
@@ -105,7 +41,6 @@ export const getTop10 = () => {
                     percent_change_24h : parseFloat(percent_change_24h).toFixed(2)
                 });
             });
-            console.log('_geTop10_ =>', response);
             resolve(response);
         } catch(error) {
             logger.error('Error while fetch data from CMC ', error)
@@ -114,3 +49,52 @@ export const getTop10 = () => {
     });
 }
 
+/**
+ * it will return the selecte dsymbols information only
+ * @param selectedSymbols 
+ * @returns 
+ */
+export const getSelectedList = (selectedSymbols: String):Promise<any> => {
+    return new Promise(async(resolve, reject): Promise<any> => {
+        try {
+            let response = [];
+            let basePair = `USD`;
+            let { data: { data }} = await call('quotes-latest', `?symbol=${selectedSymbols}&convert=${basePair}`);
+            console.log('_DEBUG_ =>', data);
+            Object.keys(data).forEach((key) => {
+                const { symbol, quote } = data[key];
+                const { price, percent_change_24h } = quote[basePair];
+                response.push({
+                    symbol: `${symbol}/${basePair}`,
+                    price: parseFloat(price).toFixed(2),
+                    percent_change_24h : parseFloat(percent_change_24h).toFixed(2)
+                });
+            });
+            resolve(response);
+        } catch(error) {
+            logger.error('Error while fetch data from CMC ', error)
+            reject(null);
+        }
+    });
+}
+
+export const getGainerLooser = (sortDirection: sortDirection) => {
+    return new Promise(async(resolve, reject) => {
+        try {let response = [];
+            let basePair = `USD`;
+            let { data: { data }} = await call('gainers-losers', `?start=1&limit=10&sort_dir=${sortDirection}&convert=${basePair}`);
+            data.forEach(({ symbol, quote }) => {
+                const { price, percent_change_24h } = quote[basePair];
+                response.push({
+                    symbol: `${symbol}/${basePair}`,
+                    price: parseFloat(price).toFixed(2),
+                    percent_change_24h : parseFloat(percent_change_24h).toFixed(2)
+                });
+            });
+            resolve(response);
+        } catch(error) {
+            logger.error('Error while fetch data from CMC ', error)
+            reject(null);
+        }
+    });
+}
