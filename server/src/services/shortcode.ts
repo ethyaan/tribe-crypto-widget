117ab943d37dcbc4199c9a7d40780bf38ed9c946
Ehsan Aghaei <ehsan.aghaeii@gmail.com>
2022-04-17 06:42:38 +0300

    refactoring commenting gainer & looser return removable in extractShortCodes update comments

--- server/src/services/shortcode.ts
+++ server/src/services/shortcode.ts
@@ -17,6 +17,7 @@ const render = (result: [any]) => {
   return out;
 };
 
+// available widgets list
 const WIDGET_LIST = {
   'top-10': {
     get: getTop10,
@@ -28,16 +29,16 @@ const WIDGET_LIST = {
     params: (v) => { return { selectedSymbols: v } },
     render
   },
-  'gainer': {
-    get: getGainerLooser,
-    params: (v) => { return { sortDirection: 'asc' } },
-    render
-  },
-  'looser': {
-    get: getGainerLooser,
-    params: (v) => { return { sortDirection: 'desc' } },
-    render
-  }
+  // 'gainer': {
+  //   get: getGainerLooser,
+  //   params: (v) => { return { sortDirection: 'asc' } },
+  //   render
+  // },
+  // 'looser': {
+  //   get: getGainerLooser,
+  //   params: (v) => { return { sortDirection: 'desc' } },
+  //   render
+  // }
 };
 
 /**
@@ -46,28 +47,36 @@ const WIDGET_LIST = {
  * @returns 
  */
 export const extractCWShortCodes = (content: string) => {
-  const regex = /\[cw\/?[^|cw]+]/gi;
+  // reg ex to find the shortcode starting like [cw] and thier properities and values
+  const regex = /\[cw\/?[^|cw]+]/g;
   const matches = content.match(regex);
-  console.log('_matches_ =>', matches);
   if (matches) {
-    const widgets = matches.map((widget) => {
-      return getWidgetTypeAndInformation(widget);
-    }).filter((element, index, array) => index === array.findIndex((item) => item.widget === element.widget));
-    return widgets;
+    // map the matches array to parsed widget information and duplicates and null widgets will be removed
+    const widgets = matches.map(getWidgetTypeAndInformation).filter((element, index, array) => index === array.findIndex((item) => {
+      return element !== null && item?.widget === element?.widget
+    }));
+    return {
+      widgets,
+      // return all the matches are removable except the ones that identifed as widget 
+      removables: matches.filter((element) => widgets.findIndex((item) => item?.code === element) === -1)
+    };
   }
   return null;
 }
 
 /**
- * get the type of the widget and it's properties values 
+ * get the type of the widget and it's properties values
  * @param widgetShortCode 
  * @returns 
  */
 export const getWidgetTypeAndInformation = (widgetShortCode: string) => {
+  // loop through list of defined widgest in WIDGET_LIST constant 
   for (let widget of Object.keys(WIDGET_LIST)) {
+    // check to make sure shortCode matched word is one of the defined widgets
     if (widgetShortCode.includes(widget)) {
-      const widgetValue = getWidgetProperyValue(widgetShortCode, widget);
-      const widgetPair = getWidgetProperyValue(widgetShortCode, 'pair') || `USD`;
+      const widgetValue = getWidgetProperyValue(widgetShortCode, widget); // get shortcode value if is available 
+      const widgetPair = getWidgetProperyValue(widgetShortCode, 'pair') || `USD`; // get the pair property value if is available
+      // return object with possible properties for all matched shortcodes + widget properties
       return {
         code: widgetShortCode,
         widget,
@@ -87,9 +96,12 @@ export const getWidgetTypeAndInformation = (widgetShortCode: string) => {
  * @returns 
  */
 export const getWidgetProperyValue = (widgetShortCode: string, property: string): string | null => {
-  const parts = widgetShortCode.replace(/\[|\]|["]+/gi, '').split(' ');
+  // remove slashed, ", ] sign from the propery - split the short code to pieces by space to separate each property part
+  const parts = widgetShortCode.replace(/\\|\]|["]+/gi, '').split(' ');
+  // loop through parts and check if they have value
   for (let part of parts) {
     if (part.includes(property) && part.includes('=')) {
+      // split the propery by = and get the value is if available
       const value = part.split('=')[1];
       return value ? value : '';
     }
